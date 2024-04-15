521a2e9bbf8cc32e76c22776289a57ace26ec9c0
Rangel Shishkov <rshishkov01@gmail.com>
2024-04-15 15:32:36 +0300

    login, register endpoints, hash password before saving in db

---
+++ Workshop-Cubicle/src/controllers/userController.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const userService = require("../services/userService");
+
+router.get("/register", (req, res) => {
+  res.render("user/register");
+});
+
+router.post("/register", async (req, res) => {
+  const { username, password, repeatPassword } = req.body;
+  userService.register({username, password, repeatPassword});
+  res.redirect("/users/login");
+});
+
+router.get("/login", (req, res) => {
+  res.render("user/login");
+});
+module.exports = router;
---
+++ Workshop-Cubicle/src/services/userService.js
@@ -0,0 +1,4 @@
+const User = require("../models/User");
+exports.register = (userData) => {
+  return User.create(userData);
+};
